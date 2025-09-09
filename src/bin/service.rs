82847bd797b2ce1dbb914f49a06213cc458b5c1d
Axel Denis <axel.denis@epitech.eu>
2025-09-09 14:29:22 +0200

    feat(service): added is_terminal check to ignore ctrl-d when is service

--- src/bin/service.rs
+++ src/bin/service.rs
@@ -19,6 +19,7 @@ use std::collections::HashMap;
  *  and execute instructions on the disk
  */
 use std::env;
+use std::io::IsTerminal;
 
 use futures_util::stream::SplitSink;
 use futures_util::{SinkExt, StreamExt};
@@ -415,13 +416,20 @@ async fn main() {
     }
 
     let ip_string = env::args().nth(1);
-    let terminal_handle = tokio::spawn(terminal_watchdog(interrupt_tx));
+    let terminal_handle = if std::io::stdout().is_terminal() {
+        Some(tokio::spawn(terminal_watchdog(interrupt_tx)))
+    } else {
+        None
+    };
     let signals_task = tokio::spawn(handle_signals(signals_tx, interrupt_rx));
     log::trace!("Starting service on {:?}", ip_string);
     log::info!("Started");
     let _ = start_cli_listener(&mut pods, ip_string, signals_rx).await;
 
-    terminal_handle.abort();
+    if let Some(terminal_handle) = terminal_handle {
+        terminal_handle.abort();
+    }
+
     signals_task.await.unwrap();
 
     log::info!("Stopping");
