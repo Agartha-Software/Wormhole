88020a59bcc6bd984adbdfea6f43eb8e929c36e1
Axel Denis <axel.denis@epitech.eu>
2025-10-10 15:02:22 +0200

    feat: updated templates.rs with PathBufs (cli)

--- src/commands/cli/templates.rs
+++ src/commands/cli/templates.rs
@@ -2,15 +2,16 @@
 // In code we trust
 // AgarthaSoftware - 2024
 use std::fs;
+use std::path::PathBuf;
 
 use crate::config::types::Config;
 use crate::config::{GlobalConfig, LocalConfig};
 use crate::error::CliResult;
 use crate::pods::arbo::{GLOBAL_CONFIG_FNAME, LOCAL_CONFIG_FNAME};
-use crate::pods::whpath::WhPath;
 
 #[must_use]
-pub fn templates(path: &WhPath, name: &str) -> CliResult<String> {
+pub fn templates(path: &PathBuf, _name: &str) -> CliResult<String> {
+    // TODO - name unused
     let global_config = GlobalConfig::default();
     let local_config = LocalConfig::default();
     fs::read_dir(path).map(|_| ())?;
