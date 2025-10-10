0db3f4d8ca1f407edaf294761e8b1e3fe66c7078
Axel Denis <axel.denis@epitech.eu>
2025-10-10 14:26:22 +0200

    feat: changed type declaration of disk managers to use PathBufs

--- src/pods/disk_managers/mod.rs
+++ src/pods/disk_managers/mod.rs
@@ -1,7 +1,7 @@
 use std::io;
 
-use super::whpath::WhPath;
 use std::fmt::Debug;
+use std::path::PathBuf;
 pub mod dummy_disk_manager;
 #[cfg(target_os = "linux")]
 pub mod unix_disk_manager;
@@ -12,25 +12,25 @@ pub struct DiskSizeInfo {
 }
 
 pub trait DiskManager: Send + Sync + Debug {
-    fn log_arbo(&self, path: &WhPath) -> io::Result<()>;
+    fn log_arbo(&self, path: &PathBuf) -> io::Result<()>;
 
-    fn new_file(&self, path: &WhPath, permissions: u16) -> io::Result<()>;
+    fn new_file(&self, path: &PathBuf, permissions: u16) -> io::Result<()>;
 
-    fn set_permisions(&self, path: &WhPath, permissions: u16) -> io::Result<()>;
+    fn set_permisions(&self, path: &PathBuf, permissions: u16) -> io::Result<()>;
 
-    fn remove_file(&self, path: &WhPath) -> io::Result<()>;
+    fn remove_file(&self, path: &PathBuf) -> io::Result<()>;
 
-    fn remove_dir(&self, path: &WhPath) -> io::Result<()>;
+    fn remove_dir(&self, path: &PathBuf) -> io::Result<()>;
 
-    fn write_file(&self, path: &WhPath, binary: &[u8], offset: usize) -> io::Result<usize>;
+    fn write_file(&self, path: &PathBuf, binary: &[u8], offset: usize) -> io::Result<usize>;
 
-    fn set_file_size(&self, path: &WhPath, size: usize) -> io::Result<()>;
+    fn set_file_size(&self, path: &PathBuf, size: usize) -> io::Result<()>;
 
-    fn mv_file(&self, path: &WhPath, new_path: &WhPath) -> io::Result<()>;
+    fn mv_file(&self, path: &PathBuf, new_path: &PathBuf) -> io::Result<()>;
 
-    fn read_file(&self, path: &WhPath, offset: usize, buf: &mut [u8]) -> io::Result<usize>;
+    fn read_file(&self, path: &PathBuf, offset: usize, buf: &mut [u8]) -> io::Result<usize>;
 
-    fn new_dir(&self, path: &WhPath, permissions: u16) -> io::Result<()>;
+    fn new_dir(&self, path: &PathBuf, permissions: u16) -> io::Result<()>;
 
     fn size_info(&self) -> io::Result<DiskSizeInfo>;
 }
