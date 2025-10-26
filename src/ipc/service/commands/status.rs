184e617931eb033232b154d76cf83d269521d99a
Arthur-Aillet <arthur.aillet@epitech.eu>
2025-10-26 20:49:15 +0100

    chore: unused warning remove

--- src/ipc/service/commands/status.rs
+++ src/ipc/service/commands/status.rs
@@ -1,19 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
-
-use crate::{
-    config::{types::Config, GlobalConfig, LocalConfig},
-    ipc::{
-        answers::{NewAnswer, StatusAnswer},
-        commands::NewRequest,
-        service::connection::send_answer,
-    },
-    network::server::Server,
-    pods::{
-        arbo::{GLOBAL_CONFIG_FNAME, LOCAL_CONFIG_FNAME},
-        pod::Pod,
-        whpath::JoinPath,
-    },
-};
+use crate::ipc::{answers::StatusAnswer, service::connection::send_answer};
 
 pub async fn status<Stream>(stream: &mut Stream) -> std::io::Result<bool>
 where
