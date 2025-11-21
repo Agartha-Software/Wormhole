ac5e35a292cc7ddae1a662950dec1cf1df5a27da
Axel DENIS <axel.denis@epitech.eu>
2025-11-21 17:02:24 +0100

    fix: fixing whpatherror to winfsperror

--- src/winfsp/traits.rs
+++ src/winfsp/traits.rs
@@ -20,11 +20,12 @@ use crate::{
 use nt_time::FileTime;
 use windows::Win32::{
     Foundation::{
-        CRYPT_E_BAD_ENCODE, GENERIC_EXECUTE, GENERIC_READ, GENERIC_WRITE, STATUS_ACCESS_DENIED,
-        STATUS_DATA_ERROR, STATUS_DIRECTORY_NOT_EMPTY, STATUS_FILE_IS_A_DIRECTORY,
-        STATUS_INVALID_HANDLE, STATUS_INVALID_PARAMETER, STATUS_NETWORK_UNREACHABLE,
-        STATUS_NOT_A_DIRECTORY, STATUS_OBJECT_NAME_EXISTS, STATUS_OBJECT_NAME_NOT_FOUND,
-        STATUS_OBJECT_PATH_NOT_FOUND, STATUS_PENDING, STATUS_POSSIBLE_DEADLOCK,
+        CRYPT_E_BAD_ENCODE, GENERIC_EXECUTE, GENERIC_READ, GENERIC_WRITE, NTSTATUS,
+        STATUS_ACCESS_DENIED, STATUS_DATA_ERROR, STATUS_DIRECTORY_NOT_EMPTY,
+        STATUS_FILE_IS_A_DIRECTORY, STATUS_INVALID_DEVICE_REQUEST, STATUS_INVALID_HANDLE,
+        STATUS_INVALID_PARAMETER, STATUS_NETWORK_UNREACHABLE, STATUS_NOT_A_DIRECTORY,
+        STATUS_OBJECT_NAME_EXISTS, STATUS_OBJECT_NAME_NOT_FOUND, STATUS_OBJECT_PATH_NOT_FOUND,
+        STATUS_PENDING, STATUS_POSSIBLE_DEADLOCK,
     },
     Storage::FileSystem::{
         FILE_ATTRIBUTE_ARCHIVE, FILE_ATTRIBUTE_DIRECTORY, FILE_WRITE_ATTRIBUTES, SYNCHRONIZE,
@@ -70,15 +71,19 @@ impl From<WhError> for FspError {
             WhError::NetworkDied { called_from: _ } => STATUS_NETWORK_UNREACHABLE.into(),
             WhError::WouldBlock { called_from: _ } => STATUS_PENDING.into(),
             WhError::InodeIsADirectory => STATUS_FILE_IS_A_DIRECTORY.into(),
-            WhError::WhPathError { e: _ } => CRYPT_E_BAD_ENCODE.into(), // REVIEW unsure about this type
+            WhError::ConversionError => CRYPT_E_BAD_ENCODE.into(), // REVIEW unsure about this type
         }
     }
 }
 
 impl From<WhPathError> for FspError {
-    fn from(value: WhPathError) -> Self {
-        let wherr: WhError = value.into();
-        wherr.into()
+    fn from(e: WhPathError) -> Self {
+        match e {
+            WhPathError::NotRelative => STATUS_INVALID_DEVICE_REQUEST.into(),
+            WhPathError::NotValidUtf8 => CRYPT_E_BAD_ENCODE.into(),
+            WhPathError::NotNormalized => STATUS_INVALID_DEVICE_REQUEST.into(),
+            WhPathError::InvalidOperation => STATUS_INVALID_DEVICE_REQUEST.into(),
+        }
     }
 }
 
