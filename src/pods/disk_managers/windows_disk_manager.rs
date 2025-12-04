d31db0ed0985db6f0b91a04ef3ba1428f79f7209
Axel Denis <axel.denis@outlook.fr>
2025-12-04 09:40:43 +0100

    feat: removing old win disk error pritns

--- src/pods/disk_managers/windows_disk_manager.rs
+++ src/pods/disk_managers/windows_disk_manager.rs
@@ -49,24 +49,21 @@ impl DiskManager for WindowsDiskManager {
         log::trace!("Stop of WindowsDiskManager");
 
         self.stopped = true;
-        std::fs::rename(&self.mount_point, &self.original_location).inspect_err(|e| log::error!("WindowsDiskManager was unable to move the directory to its initial location (on stop): {e}"))?;
+        std::fs::rename(&self.mount_point, &self.original_location)?;
         Ok(())
     }
 
     fn new_file(&self, path: &WhPath, _permissions: u16) -> io::Result<()> {
-        std::fs::File::create(&self.mount_point.join(path))
-            .inspect_err(|e| log::error!("WDM::new_file Error: {e}"))?;
+        std::fs::File::create(&self.mount_point.join(path))?;
         Ok(())
     }
 
     fn remove_file(&self, path: &WhPath) -> io::Result<()> {
         std::fs::remove_file(&self.mount_point.join(path))
-            .inspect_err(|e| log::error!("WDM::remove_file Error: {e}"))
     }
 
     fn remove_dir(&self, path: &WhPath) -> io::Result<()> {
         std::fs::remove_dir(&self.mount_point.join(path))
-            .inspect_err(|e| log::error!("WDM::remove_dir Error: {e}"))
     }
 
     fn write_file(&self, path: &WhPath, binary: &[u8], offset: usize) -> io::Result<usize> {
@@ -74,7 +71,6 @@ impl DiskManager for WindowsDiskManager {
             .write(true)
             .open(&self.mount_point.join(path))?
             .seek_write(binary, offset as u64)
-            .inspect_err(|e| log::error!("WDM::write_file Error: {e}"))
     }
 
     fn set_file_size(&self, path: &WhPath, size: usize) -> io::Result<()> {
@@ -82,7 +78,6 @@ impl DiskManager for WindowsDiskManager {
             .write(true)
             .open(&self.mount_point.join(path))?
             .set_len(size as u64)
-            .inspect_err(|e| log::error!("WDM::set_file_size Error: {e}"))
     }
 
     fn mv_file(&self, path: &WhPath, new_path: &WhPath) -> io::Result<()> {
@@ -92,18 +87,15 @@ impl DiskManager for WindowsDiskManager {
             &self.mount_point.join(path),
             &self.mount_point.join(new_path),
         )
-        .inspect_err(|e| log::error!("WDM::mv_file Error: {e}"))
     }
 
     fn read_file(&self, path: &WhPath, offset: usize, buf: &mut [u8]) -> io::Result<usize> {
         std::fs::File::open(&self.mount_point.join(path))?
             .seek_read(buf, offset as u64)
-            .inspect_err(|e| log::error!("WDM::read_file Error: {e}"))
     }
 
     fn new_dir(&self, path: &WhPath, _permissions: u16) -> io::Result<()> {
         std::fs::create_dir(&self.mount_point.join(path))
-            .inspect_err(|e| log::error!("WDM::new_dir Error: {e}"))
     }
 
     fn size_info(&self) -> std::io::Result<super::DiskSizeInfo> {
@@ -111,7 +103,6 @@ impl DiskManager for WindowsDiskManager {
             free_size: fs2::free_space(&self.mount_point)? as usize,
             total_size: fs2::total_space(&self.mount_point)? as usize,
         })
-        .inspect_err(|e| log::error!("WDM::size_info Error: {e}"))
     }
 
     fn set_permisions(&self, _path: &WhPath, _permissions: u16) -> io::Result<()> {
