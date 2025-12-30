b7931fde9a91d4605b1e7995a321cb45c8494958
Arthur-Aillet <arthur.aillet@epitech.eu>
2025-12-30 16:30:56 +0100

    chore: remove deadcode

--- src/config/local_file.rs
+++ src/config/local_file.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-use crate::error::CliError;
-
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct LocalConfigFile {
     pub general: GeneralLocalConfig,
@@ -12,17 +10,4 @@ pub struct GeneralLocalConfig {
     pub name: Option<String>,
     pub hostname: Option<String>,
     pub public_url: Option<String>,
-}
-
-impl LocalConfigFile {
-    pub fn constructor(&mut self, local: Self) -> Result<(), CliError> {
-        // self.general.name = local.general.name;
-        if local.general.hostname != self.general.hostname {
-            log::warn!("Local Config: Impossible to modify an ip address");
-            return Err(CliError::Unimplemented {
-                arg: "Local Config: Impossible to modify an ip address".to_owned(),
-            });
-        }
-        Ok(())
-    }
 }
