37578f02aaed25a5c506cf8f1efcfd11d8284766
Axel Denis <axel.denis@outlook.fr>
2026-01-09 15:29:23 +0100

    refactor: moved web api logic in tcp file

--- src/ipc/service/mod.rs
+++ src/ipc/service/mod.rs
@@ -6,25 +6,20 @@ mod tcp;
 use crate::{
     ipc::{
         commands::Command,
-        error::{ListenerError, TCPListenerError},
+        error::ListenerError,
         service::{
             connection::{handle_command, handle_connection},
             socket::new_socket_listener,
-            tcp::{create_tcp_socket, new_free_tcp_listener},
+            tcp::{create_tcp_socket, handle_tcp_connection, rest_app_handler},
         },
     },
     pods::pod::Pod,
 };
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
-use either::Either;
-use interprocess::local_socket::{self, traits::tokio::Listener};
+use interprocess::local_socket::traits::tokio::Listener;
 use std::{collections::HashMap, future::IntoFuture};
-use tokio::{
-    net::TcpListener,
-    sync::{
-        mpsc::{self, UnboundedReceiver},
-        oneshot,
-    },
+use tokio::sync::{
+    mpsc::{self, UnboundedReceiver},
+    oneshot,
 };
 
 pub static SOCKET_DEFAULT_NAME: &str = "wormhole.sock";
@@ -38,62 +33,37 @@ pub async fn start_commands_listeners(
     specific_socket: Option<String>,
     mut signals_rx: UnboundedReceiver<()>,
 ) -> Result<(), ListenerError> {
-    let (tcp_listener, ip) = create_tcp_socket(specific_ip).await?;
+    // SECTION WEB API
 
+    let (tcp_listener, ip) = create_tcp_socket(specific_ip).await?;
     println!("Started Tcp Listener at '{}'", ip.to_string());
 
     let (tx, mut rx) = mpsc::channel::<(Command, oneshot::Sender<String>)>(100);
-
     let rest_app = axum::Router::new()
         .route("/", axum::routing::get(rest_app_handler))
         .with_state(tx);
     let rest_service = tokio::spawn(axum::serve(tcp_listener, rest_app).into_future());
 
+    // !SECTION
+
+    // SECTION OS-PIPES
+
     let socket_name = specific_socket.unwrap_or(SOCKET_DEFAULT_NAME.to_string());
     let socket_listener = new_socket_listener(&socket_name)?;
     println!("Started Socket Listener at '{}'", socket_name);
+
+    // !SECTION
+
     println!("Wormhole running!");
 
     loop {
         if tokio::select! {
             Ok(stream) = socket_listener.accept() => handle_connection(pods, stream).await,
-            Some((command, reply_tx)) = rx.recv() => {
-                let mut buffer = String::new();
-                let mut stream = Either::Right(&mut buffer);
-                let _ = handle_command::<local_socket::tokio::Stream>(command, pods, &mut stream).await;
-                let _ = reply_tx.send(buffer);
-                false
-            },
+            Some((command, reply_tx)) = rx.recv() => handle_tcp_connection(command, reply_tx, pods).await,
             _ = signals_rx.recv() => true,
         } {
             rest_service.abort();
             return Ok(());
         };
-    }
-}
-
-async fn rest_app_handler(
-    State(tx): State<mpsc::Sender<(Command, oneshot::Sender<String>)>>,
-    Json(payload): Json<serde_json::Value>,
-) -> impl IntoResponse {
-    let command: Result<Command, serde_json::Error> = serde_json::from_value(payload);
-
-    let command = match command {
-        Err(_) => return StatusCode::BAD_REQUEST.into_response(),
-        Ok(c) => c,
-    };
-
-    let (reply_tx, reply_rx) = oneshot::channel();
-    if tx.send((command, reply_tx)).await.is_err() {
-        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
-    }
-
-    match reply_rx.await {
-        Ok(response) => (
-            [(axum::http::header::CONTENT_TYPE, "application/json")],
-            response,
-        )
-            .into_response(),
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
 }
--- src/ipc/service/tcp.rs
+++ src/ipc/service/tcp.rs
@@ -1,5 +1,15 @@
-use crate::{ipc::error::TCPListenerError, network::ip::IpP};
-use tokio::net::TcpListener;
+use crate::ipc::service::handle_command;
+use crate::pods::pod::Pod;
+use crate::{
+    ipc::{commands::Command, error::TCPListenerError},
+    network::ip::IpP,
+};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use either::Either;
+use interprocess::local_socket;
+use std::collections::HashMap;
+use tokio::sync::mpsc;
+use tokio::{net::TcpListener, sync::oneshot};
 
 const MAX_TRY_PORTS: u16 = 10;
 const MAX_PORT: u16 = 65535;
@@ -43,5 +53,46 @@ async fn new_free_tcp_listener() -> Result<(TcpListener, String), TCPListenerErr
                 port_tries_count += 1;
             }
         }
+    }
+}
+
+/// Borrow pods, execute the command, sends the answer to the web api
+pub async fn handle_tcp_connection(
+    command: Command,
+    reply_tx: oneshot::Sender<String>,
+    pods: &mut HashMap<String, Pod>,
+) -> bool {
+    let mut buffer = String::new();
+    let mut stream = Either::Right(&mut buffer);
+    let _ = handle_command::<local_socket::tokio::Stream>(command, pods, &mut stream).await;
+    let _ = reply_tx.send(buffer);
+    false
+}
+
+// As the rest_app server is a forever going task, it can't borrow pods
+// Instead it uses a tx to trigger the same select! as the os-pipe handler
+pub async fn rest_app_handler(
+    State(tx): State<mpsc::Sender<(Command, oneshot::Sender<String>)>>,
+    Json(payload): Json<serde_json::Value>,
+) -> impl IntoResponse {
+    let command: Result<Command, serde_json::Error> = serde_json::from_value(payload);
+
+    let command = match command {
+        Err(_) => return StatusCode::BAD_REQUEST.into_response(),
+        Ok(c) => c,
+    };
+
+    let (reply_tx, reply_rx) = oneshot::channel();
+    if tx.send((command, reply_tx)).await.is_err() {
+        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+    }
+
+    match reply_rx.await {
+        Ok(response) => (
+            [(axum::http::header::CONTENT_TYPE, "application/json")],
+            response,
+        )
+            .into_response(),
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
 }
