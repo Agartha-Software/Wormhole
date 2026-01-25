38972caf6abcb237d04996725011178eda5e2a95
Arthur-Aillet <arthur.aillet@epitech.eu>
2026-01-25 13:37:20 +0100

    fix: unfreeze remove frozen pod only after the new succeded

--- src/service/commands/unfreeze.rs
+++ src/service/commands/unfreeze.rs
@@ -17,8 +17,8 @@ impl Service {
         Stream: tokio::io::AsyncWrite + tokio::io::AsyncRead + Unpin,
     {
         log::error!("frozen: {:?}; pods: {:?}", self.frozen_pods, self.pods);
-        let name = match find_frozen_pod(&id, &self.frozen_pods) {
-            Some((name, _)) => name,
+        let (name, proto) = match find_frozen_pod(&id, &self.frozen_pods) {
+            Some(found) => found,
             None => {
                 return match find_pod(&id, &self.pods) {
                     Some(_) => send_answer(UnfreezeAnswer::AlreadyUnfrozen, stream),
@@ -28,12 +28,6 @@ impl Service {
             }
         };
 
-        let name = name.clone();
-        let proto = self
-            .frozen_pods
-            .remove(&name)
-            .expect("Already checked that the frozen_pod exist");
-
         let server = match Server::from_specific_address(proto.bound_socket) {
             Ok(server) => Arc::new(server),
             Err(err) => {
@@ -41,12 +35,17 @@ impl Service {
             }
         };
 
-        match Pod::new(proto, server).await {
+        match Pod::new(proto.clone(), server).await {
             Ok(pod) => self.pods.insert(name.clone(), pod),
             Err(err) => {
                 return send_answer(UnfreezeAnswer::PodCreationFailed(err.into()), stream).await;
             }
         };
+
+        let name = name.clone();
+        self.frozen_pods
+            .remove(&name)
+            .expect("Already checked that the frozen_pod exist");
 
         send_answer(UnfreezeAnswer::Success(name), stream).await
     }
