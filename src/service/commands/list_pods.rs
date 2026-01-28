6470b4cf350968522e8cdf3f5514a70cf69b3e2e
Axel Denis <axel.denis@outlook.fr>
2026-01-28 17:19:54 +0100

    fix: fixed 3 new commands service side

--- src/service/commands/list_pods.rs
+++ src/service/commands/list_pods.rs
@@ -1,16 +1,17 @@
-use std::collections::HashMap;
+use crate::{ipc::answers::ListPodsAnswer, service::{Service, connection::send_answer}};
 
-use crate::{ipc::answers::ListPodsAnswer, pods::pod::Pod, service::connection::send_answer};
-
-pub async fn list_pods<Stream>(
-    pods: &HashMap<String, Pod>,
-    stream: &mut either::Either<&mut Stream, &mut String>,
-) -> std::io::Result<bool>
-where
+impl Service {
+    pub async fn list_pods<Stream>(
+        &self,
+        stream: &mut either::Either<&mut Stream, &mut String>,
+    ) -> std::io::Result<()>
+    where
     Stream: tokio::io::AsyncWrite + tokio::io::AsyncRead + Unpin,
-{
-    let list: Vec<String> = pods.iter().map(|(name, _)| name).cloned().collect();
-
-    send_answer(ListPodsAnswer::Pods(list), stream).await?;
-    Ok(false)
+    {
+        let list: Vec<String> = self.pods.iter().map(|(name, _)| name).cloned().collect();
+        
+        send_answer(ListPodsAnswer::Pods(list), stream).await?;
+        Ok(())
+    }
 }
+    
--- src/service/commands/stats_per_filetype.rs
+++ src/service/commands/stats_per_filetype.rs
@@ -1,34 +1,34 @@
-use std::collections::HashMap;
-
 use crate::{
     ipc::{answers::StatsPerFiletypeAnswer, commands::PodId},
-    pods::pod::Pod,
-    service::{commands::find_pod, connection::send_answer},
+    service::{Service, commands::find_pod, connection::send_answer},
 };
 
-pub async fn stats_per_filetype<Stream>(
-    pod: PodId,
-    pods: &HashMap<String, Pod>,
-    stream: &mut either::Either<&mut Stream, &mut String>,
-) -> std::io::Result<bool>
-where
+impl Service {
+    pub async fn stats_per_filetype<Stream>(
+        &self,
+        pod: PodId,
+        stream: &mut either::Either<&mut Stream, &mut String>,
+    ) -> std::io::Result<()>
+    where
     Stream: tokio::io::AsyncWrite + tokio::io::AsyncRead + Unpin,
-{
-    match find_pod(&pod, pods) {
-        Some((_, pod)) => {
-            let stats = pod.get_stats_per_filetype();
-
-            send_answer(
-                match stats {
-                    Ok(i) => StatsPerFiletypeAnswer::Stats(i),
-                    Err(_) => StatsPerFiletypeAnswer::InternalError,
-                },
-                stream,
-            )
-            .await?
-        }
-        None => send_answer(StatsPerFiletypeAnswer::PodNotFound, stream).await?,
-    };
-
-    Ok(false)
+    {
+        match find_pod(&pod, &self.pods) {
+            Some((_, pod)) => {
+                let stats = pod.get_stats_per_filetype();
+                
+                send_answer(
+                    match stats {
+                        Ok(i) => StatsPerFiletypeAnswer::Stats(i),
+                        Err(_) => StatsPerFiletypeAnswer::InternalError,
+                    },
+                    stream,
+                )
+                .await?
+            }
+            None => send_answer(StatsPerFiletypeAnswer::PodNotFound, stream).await?,
+        };
+        
+        Ok(())
+    }
 }
+    
