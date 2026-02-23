5b4a8a47478db35700d75bcea2a4ad2560b12447
syborg64 <julian.scott@epitech.eu>
2026-02-23 19:53:52 +0100

    fix(p2p): log requests # on debug

--- src/pods/network/event_loop.rs
+++ src/pods/network/event_loop.rs
@@ -220,14 +220,14 @@ impl EventLoop {
         status: oneshot::Sender<Option<Response>>,
         peer: PeerId,
     ) {
-        let log_msg = log::log_enabled!(log::Level::Trace).then_some(format!("{message}"));
+        let log_msg = log::log_enabled!(log::Level::Debug).then_some(format!("{message}"));
         let request_id = self
             .swarm
             .behaviour_mut()
             .request_response
             .send_request(permit, &peer, message);
         if let Some(log_msg) = log_msg {
-            log::trace!("Requesting {log_msg} to {peer}: #{request_id}");
+            log::debug!("Requesting {log_msg} to {peer}: #{request_id}");
         }
         self.answers.insert(request_id, status);
     }
@@ -238,6 +238,7 @@ impl EventLoop {
         message: Request,
         to: I,
     ) {
+        let log_msg = log::log_enabled!(log::Level::Debug).then_some(format!("{message}"));
         let mut log_to = vec![];
         let to = to.into_iter();
         let permits = permits.into_iter();
@@ -259,18 +260,21 @@ impl EventLoop {
                 }
             }
 
-            if log_to.len() > 1 {
-                log::debug!("Broadcasting {message} to {:?}", &log_to[..]);
-            } else {
-                log::debug!("Sending {message} to {:?}", first.to_base58());
-            }
 
             // let it be moved in here
-            self.swarm.behaviour_mut().request_response.send_request(
+            let first_id = self.swarm.behaviour_mut().request_response.send_request(
                 first_permit,
                 first.deref(),
                 message,
             );
+
+            if let Some(log_msg) = log_msg {
+                if log_to.len() > 1 {
+                    log::debug!("Broadcasting {log_msg} to {:?}", &log_to[..]);
+                } else {
+                    log::debug!("Sending {log_msg} to {:?}: #{first_id}", first.to_base58());
+                }
+            }
         }
     }
 
